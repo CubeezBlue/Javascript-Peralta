6409e2a12b3a1db0809f694c085863e2783bddde
Nicolas Peralta <nicomartin98@hotmail.com>
2023-07-30 19:43:14 -0300

    agregados

--- js/main.js
+++ js/main.js
@@ -1,66 +1,70 @@
 // Contenido Principal
 
-//Varriables
-let opcion = 0
+const Alumonos = [{
+    nombre: "",
+    apellido: "",
+    nota1: 0,
+    nota2: 0,
+    nota3: 0,
+    condicion: ""
+}]
 
-
-let nota1 = 0
-let nota2 = 0
-let nota3 = 0
-
-//Menu
-do {
-
-    opcion = parseInt(prompt("Evaluador de Notas \nPara Ingresar a la opcion deseada ingrese el numero correspondiente \n1- Ingresar notas de los tres examenes de un alumno \n2- Ver las tres notas del alumno \n3- Ver Promedio final del alumno y su condicion \n4- Salir"))
-
-    switch (opcion) {
-        case 1:
-            ingDeNotas()
-            break;
-        case 2:
-            verNotas()
-            break;
-        case 3:
-            promedioNotas()
-            break;
-        case 4:
-            break;
-        default:
-            alert("Ingrese una opcion correcta")
-    }
-} while (opcion != 4)
-
-
-//Funcion Ingreso de Notas
-function ingDeNotas() {
-    let i = 0
-    for (i; i < 4; i++) {
-        if (i == 1) {
-            nota1 = parseInt(prompt("Ingrese la primera nota: "))
+const ingAlumno = () => {
+    Alumonos.forEach(el => {
+        while (el.nombre == "" || el.apellido == "") {
+            el.nombre = prompt("Ingrese el Nombre del alumno: ")
+            el.apellido = prompt("Ingrese el Apellido del alumno: ")
         }
-        if (i == 2) {
-            nota2 = parseInt(prompt("Ingrese la segunda nota: "))
+        el.nota1 = parseInt(prompt("Ingrese la primera nota del alumno"))
+        el.nota2 = parseInt(prompt("Ingrese la segunda nota del alumno"))
+        el.nota3 = parseInt(prompt("Ingrese la tercera nota del alumno"))
+
+
+        if (el.nota1 < 4 || el.nota2 < 4 || el.nota3 < 4) {
+            el.condicion = "Desaprobado"
+        } else {
+            el.condicion = "Aprobado"
         }
-        if (i == 3) {
-            nota3 = parseInt(prompt("Ingrese la tercera nota: "))
-        }
-    }
+    })
 }
 
-// Funcion Ver Notas
-function verNotas() {
-    alert("Notas de los tres examenes \nPrimer Examen: " + nota1 + "\nSegundo Examen: " + nota2 + "\nTercer Examen: " + nota3)
+const verAlumno = () => {
+    let opcion = 0
+    do {
 
+        opcion = parseInt(prompt("Evaluador de Notas \nPara Ingresar a la opcion deseada ingrese el numero correspondiente \n1- Ver las tres notas del alumno \n2- Ver Promedio final del alumno y su condicion \n3- Salir"))
+
+        switch (opcion) {
+
+            case 1:
+                verNotas()
+                break;
+            case 2:
+                promedioNotas()
+                break;
+            case 3:
+                break;
+            default:
+                alert("Ingrese una opcion correcta")
+        }
+    } while (opcion != 3)
+}
+
+const verNotas = () => {
+    Alumonos.map(Alumonos => {
+        alert("La primera nota del alumno es: " + Alumonos.nota1 + "\nSegunda nota: " +
+            Alumonos.nota2 + "\nTercera nota: " + Alumonos.nota3)
+    })
 }
 
 
-// Funcion Ver Promedio 
-function promedioNotas() {
-    let promedio = (nota1 + nota2 + nota3) / 3
-    if (nota1 < 4 || nota2 < 4 || nota3 < 4) {
-        alert("El promedio del alumno es de: " + promedio + "\n Y su condicion es: Libre \n(Porque desaprobo algun examen)")
-
-    } else {
-        alert("El promedio del alumno es de: " + promedio + "\n Y su condicion es: Regular")
-    }
+const promedioNotas = () => {
+    Alumonos.forEach(el => {
+        let promedio = (el.nota1 + el.nota2 + el.nota3) / 3
+        alert("El promedio del alumno " + el.nombre + " " + el.apellido + " es: " + promedio + "\nY su condicion es de: " + el.condicion)
+    })
 }
+
+
+ingAlumno()
+verAlumno()
